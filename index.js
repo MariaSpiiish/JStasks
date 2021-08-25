38861945cc6cb156c26a91f8e6c19dad4add0ef8
Ivan Lapshin <ivan.lapshin@fastdev.se>
2021-08-25 18:32:26 +0400

    created index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let text = 'JavaScript';
+let lowerStr = text.toLowerCase();
+let textToArr = lowerStr.split('');
+
+let alphabet = ' abcdefghijklmnopqrstuvwxyz';
+let alphabetToArr = alphabet.split('');
+
+let result = textToArr.map(elem => alphabetToArr.indexOf(elem));
+
+
+console.log(result);
