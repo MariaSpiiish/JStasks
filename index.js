51a7aae7ba0bcba4e35d85f7031a6385914ed847
Dmitry Kouznetsov <dmitry.kouznetsov@gmail.com>
2021-09-01 16:04:06 +0200

    asdf

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // @flow
 let text /*: string */ = ('JavaScript' /*: string */);
-let lowerStr /*: string */ = (text.toLowerCase() /*: string */);
+let lowerStr /*: string */ = ((text /*: ?this_is_a_value? */).(toLowerCase/*: ?and_this_one_kinda_too_even_though_surrounding_it_with_()_brakes_the_syntax? */)() /*: string */);
 let textToArr /*: Array<string> */ = lowerStr.split('');
 
 let alphabet /*: string */ = ' abcdefghijklmnopqrstuvwxyz';
