3cca84066b8ea3573125b02100b27e2d6159837c
unknown <mariaspiiish@gmail.com>
2021-09-01 17:18:31 +0400

    added type annotations to all my lets

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // @flow
 let text /*: string */ = 'JavaScript';
-let lowerStr = text.toLowerCase();
-let textToArr = lowerStr.split('');
+let lowerStr /*: string */ = text.toLowerCase();
+let textToArr /*: Array<string> */ = lowerStr.split('');
 
-let alphabet = ' abcdefghijklmnopqrstuvwxyz';
-let alphabetToArr = alphabet.split('');
+let alphabet /*: string */ = ' abcdefghijklmnopqrstuvwxyz';
+let alphabetToArr /*: Array<string> */ = alphabet.split('');
 
-let result = textToArr.map(elem => alphabetToArr.indexOf(elem));
+let result /*: Array<number> */= textToArr.map(elem => alphabetToArr.indexOf(elem));
 
 console.log(result);
